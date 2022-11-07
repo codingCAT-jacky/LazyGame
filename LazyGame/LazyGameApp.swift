a405a38f1d534b2c9a481f8772d107a6bec3dc9d
kevinwforney <kevinwilliamforney@gmail.com>
2022-11-07 20:39:12 +0800

    Initial Commit

---
+++ LazyGame/LazyGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  LazyGameApp.swift
+//  LazyGame
+//
+//  Created by User15 on 11/7/22.
+//
+
+import SwiftUI
+
+@main
+struct LazyGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
